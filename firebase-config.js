c04464833305cf7e0c40ab3d6be2222aad5d2815
sonusharmagaur-blip <sonu.sharma.gaur@gmail.com>
2026-09-01 17:36:57 +0530

    Update firebase-config.js for Railway HRMS test release

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,2 @@
-// Online two-phone sync ke liye Firebase project ki web config yahan paste karein.
-// Config na hone par app device-local demo mode mein chalegi.
-export const firebaseConfig = null;
+export const firebaseConfig={apiKey:'AIzaSyB_6IMi8nimjmWCHwTrYkLVj5qgQTL-l88',authDomain:'rw-9f3a7c21.firebaseapp.com',databaseURL:'https://rw-9f3a7c21-default-rtdb.asia-southeast1.firebasedatabase.app',projectId:'rw-9f3a7c21',storageBucket:'rw-9f3a7c21.firebasestorage.app',messagingSenderId:'427924717581',appId:'1:427924717581:web:f7f2b787a687aed376d544'};
+
